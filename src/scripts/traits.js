7d4f5e062c8a5ab458d211ab56c4eb07014f0c41
sam-kim99 <samkim0451@gmail.com>
2024-03-07 20:03:41 -0500

    start adding descriptions to originTraits array

--- src/scripts/traits.js
+++ src/scripts/traits.js
@@ -1,5 +1,10 @@
-const originTraits = ['8-bit', 'country', 'disco', 'edm', 'emo', 'heartsteel', 'hyperpop','illbeats', 'jazz', 'k/da', 'maestro', 'mixmaster', 'pentakill', 'punk', 'true damage', 'wildcard']
+// const originTraits = ['8-bit', 'country', 'disco', 'edm', 'emo', 'heartsteel', 'hyperpop','illbeats', 'jazz', 'k/da', 'maestro', 'mixmaster', 'pentakill', 'punk', 'true damage', 'wildcard']
 
 const classTraits = ['big shot', 'breakout', 'bruiser', 'crowd diver', 'dazzler', 'edgelord', 'executioner', 'guardian', 'mosher', 'rapidfire', 'sentinel', 'spellweaver', 'superfan']
 
-fetch('https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/tft-trait.json')
+const originTraits = {
+    '8-bit': {
+        'desc': "Emo Champions' Ability cost less Mana to cast, and they gain Mana whenever an allied champion dies.<p>(2) 20% less, gain 20 Mana<br>(4) 30% less, gain 30 Mana<br>(6) +20 AP, 40% less, gain 40 Mana"
+        
+    }
+}
