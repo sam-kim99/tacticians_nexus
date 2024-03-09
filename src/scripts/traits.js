36b858471cd3aeed07f3b3d8a9bdf93612f5384d
sam-kim99 <samkim0451@gmail.com>
2024-03-09 14:04:06 -0500

    add names to each trait object and add styling placeholder for traits

--- src/scripts/traits.js
+++ src/scripts/traits.js
@@ -1,120 +1,149 @@
 export const originTraits = {
-    '8-Bit': {
+    '8-bit': {
+        'name': '8-Bit',
         'desc': "<p>Keep score of your team\'s damage dealt. For each high score you beat, 8-bit champions gain Attack Damage.<br><br>(2) 5% AD<br>(4) 9% AD<br>(6) 14% AD and unlock a final high score, that rewards you with a grand prize.</p><br>",
         'champions': []
     },
-    'Country': {
+    'country': {
+        'name': 'Country',
         'desc': "<p>When your team loses 30% of their Health, a Dreadsteed that empowers your team. Each Country star level increases the Dreadsteed\'s Health and Attack Damage.<br><br>(3) Dreadsteed<br>(5) Midnight Dreadsteed<br>(7) Infernal Invocation Dreadsteed<p><br>",
         'champions': []
     },
-    'Disco': {
+    'disco': {
+        'name': 'Disco',
         'desc': "<p>Summon a movable Disco Ball.<br><br>Combat start: Allies next to it gain Attack Speed and healing immediately and every 3 seconds.<br><br>(3) 5% Attack Speed, 2% max Health<br>(4) 8% Attack Speed, 3% max Health<br>(5) 2 Disco Balls, 12% Attack Speed, 3% max Health<br>(6) 20% Attack Speed, 4% max Health</p><br>",
         'champions': []
     },
-    'EDM': {
+    'edm': {
+        'name': 'EDM',
         'desc': "<p>Use the EDM selector item to choose an EDM champion and see the frequency for each.<br><br>At the selected champions\'s frequency, your EDM champions cast the selected Ability with modified effectiveness.<br><br>(2) 90% effectiveness<br>(3) 110% effectiveness<br>(4) 120% effectiveness, -1 second<br>(5) 135% effectiveness, -1 seconds</p><br>",
         'champions': [] 
     },
-    'Emo': {
+    'emo': {
+        'name': 'Emo',
         'desc': "<p>Emo Champions' Ability cost less Mana to cast, and they gain Mana whenever an allied champion dies.<br>(2) 20% less, gain 20 Mana<br>(4) 30% less, gain 30 Mana<br>(6) +20 AP, 40% less, gain 40 Mana</p><br>",
         'champions': []
     },
-    'Heartsteel': {
+    'heartsteel': {
+        'name': 'Heartsteel',
         'desc': "<p>Earn Hearts by killing enemies. Gain even more by losing player combat. Every 4 player combats, convert Hearts into powerful rewards!<br><br>(3) 1x Hearts<br>(5) 2x Hearts<br>(7) 5.5x Hearts<br>(10) 18x Hearts, Gain rewards every round and keep 80% of you Hearts!</p><br>",
         'champions': []
     },
-    'Hyperpop': {
+    'hyperpop': {
+        'name': 'Hyperpop',
         'desc': "<p>When Hyperpop champions use an Ability, they grant Mana and 4 seconds of Attack Speed to their 2 closest allies.<br><br>(1) 5 Mana and 10% Attack Speed<br>(2) 10 Mana and 20% Attack Speed<br>(3) 15 Mana and 35% Attack Speed<br>(4) 20 Mana and 60% Attack Speed</p><br>",
         'champions': []
     },
-    'ILLBEATS': {
+    'illbeats': {
+        'name': 'ILLBEATS',
         'desc': "<p>(1) Gain 2/2/8 placeable Spirit Tentacles, based on Illaoi\'s star level. Tentacles gain Illaoi\'s bonus Armor and Magic Resist.</p><br>",
         'champions': []
     },
-    'Jazz': {
+    'jazz': {
+        'name': 'Jazz',
         'desc': "<p>For each active trait (except uniques), your team gains bonus Health and deals bonus damage.<br><br>(2) 1.5% Health, 1% Damage<br>(3) 2.5% Health, 1.5% Damage<br>(4) 4% Health, 2% Damage</p><br>",
         'champions': []
     },
-    'KDA': {
+    'kda': {
+        'name': 'K/DA',
         'desc': "<p>Your team gains max Health, Ability Power, and Attack Damage if they are in a lighted hex. K/DA champions gain double!<br><br>(3) +9% max Health, +9 AP & AD<br>(5) +15% max Health, +15 AP & AD<br>(7) +27% max Health, +27 AP & AD<br>(10) +45% max Health, +45 AP & AD, 10 mana per second</p><br>",
         'champions': []
     },
-    'Maestro': {
+    'maestro': {
+        'name': 'Maestro',
         'desc': "<p>(1) The Maestro always attacks at a fixed pace, converting 1% bonus Attack Speed into 0.7% Attack Damage.</p><br>",
         'champions': []
     },
-    'Mixmaster': {
+    'mixmaster': {
+        'name': 'Mixmaster',
         'desc': "<p>(1) Choose a mode that changes the Mixmaster\'s attacks and Ability!</p><br>",
         'champions': []
     },
-    'Pentakill': {
+    'pentakill': {
+        'name': 'Pentakill',
         'desc': "<p>Pentakill champions reduce incoming damage by 15% and deal bonus damage. For each champion kill, a Pentakill champion rocks out and increases their damage bonus by 25%.<br><br>On the 5th kills, all Pentakill champions rock out and your team gains 50% Attack Speed.<br><br>(3) 15% bonus damage<br>(5) 30% bonus damage<br>(7) 45% bonus damage<br>(10) 50% damage reduction and 99% bonus damage</p><br>",
         'champions': []
     },
-    'Punk': {
+    'punk': {
+        'name': 'Punk',
         'desc': "<p>Punks gain bonus Health and Attack Damage, which increases by 1% every time you spend gold on a Shop reroll.<br><br>After Punks fight in combat, your 1st Shop reroll costs 1 gold and grants 3% bonus instead!<br><br>(2) +180 Health and +18% Attack Damage<br></br>(4) +300 Health and +30% Attack Damage<br>(6) +420 Health and +42% Attack Damage</p><br>",
         'champions': []
     },
-    'True Damage': {
+    'true damage': {
+        'name': 'True Damage',
         'desc': "<p>True Damage champions deal bonus true damage. If they are holding an item, they gain a unique Bling Bonus for their Ability.<br><br>(2) 15% damage<br>(4) 30% damage<br>(6) 45% damage<br>(9) 99% damage, Bling Bonuses go Platinum!</p><br>",
         'champions': []
     },
-    'Wildcard': {
+    'wildcard': {
+        'name': 'Wildcard',
         'desc': "<p>(1) If you win player combat, Kayn becomes the Shadow Assassin. If not, he becomes Rhaast. You receive a reward based on his form every time he kills 2 enemy champions.<br><br>Shadow Assassin: 3g<br>Rhaast: 1 player health.</p><br>",
         'champions': []
     }    
 }
 
 export const classTraits = {
-    'Big Shot': {
+    'big shot': {
+        'name': 'Big Shot',
         'desc': "<p>Big Shots gain Attack Damage. which increases for 3 seconds when they use their Ability.<br><br>(2) 10% Attack Damage, 40% after casting<br>(4) 20% Attack Damage, 60% after casting<br>(6) 40% Attack Damage, 100% after casting</p><br>",
         'champions': []
     },
-    'Breakout': {
+    'breakout': {
+        'name': 'Breakout',
         'desc': "<p>(1) Akali is a member of K/DA or True Damage depending on which trait has more fielded champions. She gains a different Ability depending on which form she takes.<p><br>",
         'champions': []
     },
-    'Bruiser': {
+    'bruiser': {
+        'name': 'Bruiser',
         'desc': "<p>Your team gains 100 Health. Bruiser gain bonus maximum Health.<br><br>(2) 20% max Health<br>(4) 45% max Health<br>(6) 90% max Health</p><br>",
         'champions': []
     },
-    'Crowd Diver': {
+    'crowd diver': {
+        'name': 'Crowd Diver',
         'desc': "<p>After Crowd Divers die, they leap onto the furthest enemy, dealing 300 magic damage. Enemies within 1 hex are Stunned for 1.5 seconds.<br><br>They also deal bonus damage, increased by 1% each second.<br><br>(2) 12% bonus damage<br>(4) 30% bonus damage<br>(6) 50% bonus damage</p><br>",
         'champions': [] 
     },
-    'Dazzler': {
+    'dazzler': {
+        'name': 'Dazzler',
         'desc': "<p>Dazzler' Ability reduce their target's damage by 10% and deal bonus magic damage over 2 seconds.<br><br>(2) 20% magic damage<br>(4) 60% magic damage<br>(6) 100% magic damage</p><br>",
         'champions': []
     },
-    'Edgelord': {
+    'edgelord': {
+        'name': 'Edgelord',
         'desc': "<p>Edgelords gain Attack Speed which doubles when their target drops target drops below 50% Health. Edgelords with 1-hex range also dash through them on their next attack.<br><br>(3) 30% Attack Speed<br>(5) 55% Attack Speed<br>(7) 85% Attack Speed</p><br>",
         'champions': []
     },
-    'Executioner': {
+    'executioner': {
+        'name': 'Executioner',
         'desc': "<p>Executioner Abilities can critically strike and they gain Critical Strike Damage. Their Critical Strike Chance is increased based on their target\'s missing Health.<br><br>(2) 5% Crit Damage, Up to 25% Crit Chance<br>(4) 15% Crit Damage, Up to 80% Crit Chance<br>(6) 30% Crit Damage, Up to 160% Crit Chance</p><br>",
         'champions': []
     },
-    'Guardian': {
+    'guardian': {
+        'name': 'Guardian',
         'desc': "<p>Once per combat at 50% Health, Guardians shield themselves and their closest ally for a percent of their max Health.<br><br>(2) 25%<br>(4) 45%<br>(6) 70%</p><br>",
         'champions': []
     },
-    'Mosher': {
+    'mosher': {
+        'name': 'Mosher',
         'desc': "<p>Moshers gain Attack Speed and Omnivamp, which increases up to 100% based on their missing Health.<br><br>Omnivamp: Heal for percentage of damage dealt<br><br>(2) 20% Attack Speed, 10% Omnivamp<br>(4) 40% Attack Speed, 15% Omnivamp<br>(6) 70% Attack Speed, 20% Omnivamp<br>(8) 125% Attack Speed, 30% Omnivamp</p><br>",
         'champions': []
     },
-    'Rapidfire': {
+    'rapidfire': {
+        'name': 'Rapidfire',
         'desc': "<p>Your team gains 10% Attack Speed.<br><br>Rapidfire champions gain more on every attack, up to 10 stacks.<br><br>(2) 4% Attack Speed<br>(4) 9% Attack Speed<br>(6) 16% Attack Speed</p><br>",
         'champions': []
     },
-    'Sentinel': {
+    'sentinel': {
+        'name': 'Sentinel',
         'desc': "<p>Your team gains Armor and Magic Resist. Sentinels gain double.<br><br>(2) +16 Armor, +16 Magic Resist<br>(4) +35 Armor, +35 Magic Resist<br>(6) +55 Armor, +55 Magic Resist<br>(8) +100 Armor, +100 Magic Resist</p><br>",
         'champions': []
     },
-    'Spellweaver': {
+    'spellweaver': {
+        'name': 'Spellweaver',
         'desc': "<p>Your team gains 15 Ability Power. Spellweavers gain more, plus extra Ability Power whenever a Spellweaver casts an Ability.<br><br>(3) 20 AP, +1 AP per cast<br>(5) 35 AP, +2 AP per cast<br>(7) 80 AP, +4 AP per cast<br>(9) 130 AP, +6 AP per cast</p><br>",
         'champions': []
     },
-    'Superfan': {
+    'superfan': {
+        'name': 'Superfan',
         'desc': "<p>Superfans improve your Headliner!<br><br>(3) Headliner gets a completed item<br>(4) Headliner gets +250 Health and +15% Omnivamp<br>(5) Item upgrades to radiant<br><br></br>Omnivamp: Heal for percentage of damage dealt<br>",
         'champions': []
     }    
@@ -122,18 +151,28 @@ export const classTraits = {
 
 export function createTraits(traitObject) {
     for (let currentTrait in traitObject) {
-    const traitDiv = document.querySelector('.traits');
-    const traitName = document.createElement('div');
-    const traitDesc = document.createElement('div');
+        const traitsDiv = document.querySelector('.traits');
+        const currentTraitDiv = document.createElement('div');
+        const traitImgDiv = document.createElement('div');
+        const traitImg = document.createElement('img');
+        const traitName = document.createElement('h2');
+        const traitDesc = document.createElement('div');
+
+        currentTraitDiv.id = currentTrait;
     
-        traitName.id = currentTrait;
-        traitName.innerText = currentTrait;
+        traitImg.src = './assets/images/trait-icons/' + currentTrait + '.png';
+        traitImgDiv.appendChild(traitImg);
+        currentTraitDiv.appendChild(traitImgDiv);
+
+        traitName.id = 'traitName';
+        traitName.innerText = traitObject[currentTrait]['name'];
+        currentTraitDiv.appendChild(traitName);
+
         traitDesc.id = 'traitDesc';
-        
         traitDesc.innerHTML = traitObject[currentTrait]['desc'];
-        
-        traitName.appendChild(traitDesc);
-        traitDiv.appendChild(traitName);
+        currentTraitDiv.appendChild(traitDesc);
+
+        traitsDiv.appendChild(currentTraitDiv);
     }
 
 }
